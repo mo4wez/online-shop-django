eef1bd3f1e77ae82df5f1131ce470e933d090004
moawezz <moawez.de@gmail.com>
2023-08-24 22:54:47 +0330

    Add test for signup and login views

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class AccountsTest(TestCase):
+
+    def test_signup_page_url(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_page_url_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_template_used(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertTemplateUsed(response, 'registration/signup.html')
+
+    def test_login_page_url(self):
+        response = self.client.get('/accounts/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page_url_by_name(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_template_used(self):
+        response = self.client.get('/accounts/login/')
+        self.assertTemplateUsed(response, 'registration/login.html')
+
+    
