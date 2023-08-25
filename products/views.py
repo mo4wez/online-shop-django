20aef8c6e47294e67c6e15900102bc8d3f9e6fc4
moawezz <moawez.de@gmail.com>
2023-08-25 18:36:28 +0330

    Add ListView and DetailView to product app

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Product
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
+
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
+    
