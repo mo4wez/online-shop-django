6d474fa5d0cb9b5226a91c0b661e3ae4d8d6041a
moawezz <moawez.de@gmail.com>
2023-08-25 22:53:41 +0330

    make product_list based on frontend file

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from environs import Env
 
+import os
+
 # environment variables
 env = Env()
 env.read_env()
@@ -152,6 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
