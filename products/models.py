195bf6ae9da5e61d0bb1fed186462bb54ac50d53
moawezz <moawez.de@gmail.com>
2023-08-25 23:52:16 +0330

    Add Comment model to product app

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 class Product(models.Model):
     title = models.CharField(max_length=32)
@@ -16,4 +17,26 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse("product_detail", kwargs={"pk": self.pk})
-    
+
+
+class Comment(models.Model):
+    PRODUCT_STAR = [
+        ('1', 'Very Bad'),
+        ('2', 'Bad'),
+        ('3', 'Normal'),
+        ('4', 'Good'),
+        ('5', 'Perfect'),
+    ]
+
+    product = models.ForeignKey(to=Product, related_name="comments", on_delete=models.CASCADE)
+    user = models.ForeignKey(to=get_user_model(), related_name="comments", on_delete=models.CASCADE)
+    text = models.TextField()
+    stars = models.CharField(max_length=10, choices=PRODUCT_STAR)
+    active = models.BooleanField(default=True)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetimr_modified = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.text
