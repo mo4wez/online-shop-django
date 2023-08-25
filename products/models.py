26c4535794303f7ccc186e23d660ddfaaafe341d
moawezz <moawez.de@gmail.com>
2023-08-25 17:23:09 +0330

    create product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.TextField(max_length=32)
+    description = models.TextField()
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
