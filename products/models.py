878fc3febd037a74693592ffe61ac20384df7956
moawezz <moawez.de@gmail.com>
2023-08-25 18:35:05 +0330

    add absoulute url to product model

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Product(models.Model):
     title = models.CharField(max_length=32)
@@ -8,3 +9,11 @@ class Product(models.Model):
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("product_detail", kwargs={"pk": self.pk})
+    
