9d242066a62f09604edb7e63ec5a29cdccdacab2
moawezz <moawez.de@gmail.com>
2023-08-26 13:59:42 +0330

    Create Commenting system for products

--- products/models.py
+++ products/models.py
@@ -37,6 +37,10 @@ class Comment(models.Model):
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
         return self.text
+
+    def get_absolute_url(self):
+        return reverse("product_detail", kwargs={"pk": self.product.id})
+    
+    
--- products/views.py
+++ products/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from django.urls import reverse_lazy
+from django.urls import reverse
 
-from .models import Product
+from .models import Product, Comment
+from .forms import CommentForm
 
 class ProductListView(generic.ListView):
     queryset = Product.objects.filter(active=True)
@@ -13,4 +14,25 @@ class ProductDetailView(generic.DetailView):
     model = Product
     template_name = 'products/product_detail.html'
     context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+
+        return context
     
+
+class CommentCreateView(generic.CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.user = self.request.user
+
+        product_id = int(self.kwargs['product_id'])
+        product = get_object_or_404(Product, id=product_id)
+        obj.product = product
+
+        return super().form_valid(form)
+
