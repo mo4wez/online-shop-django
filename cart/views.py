cf4294fcb53f07f29a088e6a9741547f113d7852
moawezz <moawez.de@gmail.com>
2023-08-30 18:07:46 +0330

    Create cart urls, views, caet detail, add to cart view

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+from products.models import Product
+from .forms import AddToCartProductForm
+from .cart import Cart
+
+
+def cart_detail_view(request):
+    cart = Cart(request)
+
+    return render(request, 'cart/cart_detail.html', {
+        'cart': cart,
+    })
+
+def add_to_cart_view(request, product_id):
+    cart = Cart(request)
+
+    product = get_object_or_404(Product, id=product_id)
+    form = AddToCartProductForm(request.POST)
+
+    if form.is_valid():
+        cleaned_data = form.cleaned_data
+        quantity = cleaned_data['quantity']
+        cart.add(product, quantity)
+
+    return redirect('cart:cart_detail')
--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 
 from .models import Product, Comment
 from .forms import CommentForm
+from cart.forms import AddToCartProductForm
 
 
 class ProductListView(generic.ListView):
@@ -19,6 +20,7 @@ class ProductDetailView(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['comment_form'] = CommentForm()
+        context['add_to_cart_form'] = AddToCartProductForm()
 
         return context
     
