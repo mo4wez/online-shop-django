47f957db3042776e992bd8d4d231bc1006eed0a4
moawezz <moawez.de@gmail.com>
2023-09-02 21:25:46 +0330

    create product counter in product detail view

--- cart/cart.py
+++ cart/cart.py
@@ -46,7 +46,6 @@ class Cart:
 
     def __iter__(self):
         product_ids = self.cart.keys()
-
         products = Product.objects.filter(id__in=product_ids)
 
         cart = self.cart.copy()
@@ -61,12 +60,11 @@ class Cart:
         return len(self.cart.keys())
     
     def clear(self):
-        del self.sessionp['cart']
+        del self.session['cart']
         self.save()
 
     def get_total_price(self):
         product_ids = self.cart.keys()
-
         products = Product.objects.filter(id__in=product_ids)
 
         return sum(product.price for product in products)
