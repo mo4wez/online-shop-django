74a404e9a897075e39b6ce58c90a75afadf8fc2f
moawezz <moawez.de@gmail.com>
2023-09-11 17:22:09 +0330

    add order models and register it

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    fields = ['order', 'product', 'quantity', 'price']
+    extra = 1
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'first_name', 'last_name', 'datetime_created', 'is_paid']
+
+    inlines = [
+        OrderItemInline,
+    ]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'quantity', 'price']
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    is_paid = models.BooleanField(default=False)
+    
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    phone_number = models.CharField(max_length=15)
+    address = models.CharField(max_length=700)
+    order_notes = models.CharField(max_length=700)
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f'Order: {self.id}'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(to=Order, related_name="items", on_delete=models.CASCADE)
+    product = models.ForeignKey(to='products.Product', related_name="order_items", on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.PositiveIntegerField()
+
+
+    def __str__(self):
+        return f'OrderItem {self.id}: Product {self.product} x {self.quantity} - {self.price}'
